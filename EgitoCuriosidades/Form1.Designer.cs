928aab5e2457d51c88f1e77d30d463b72823ab8c
Paulo3000 <1988paulosergio@gmail.com>
2021-03-25 09:38:40 -0300

    first commit

---
+++ EgitoCuriosidades/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace EgitoCuriosidades
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.persas = new System.Windows.Forms.PictureBox();
+            this.doly = new System.Windows.Forms.PictureBox();
+            this.cats = new System.Windows.Forms.PictureBox();
+            this.beard = new System.Windows.Forms.PictureBox();
+            this.fundo = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.persas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.doly)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cats)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.beard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fundo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(713, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "stopSond";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // persas
+            // 
+            this.persas.Image = global::EgitoCuriosidades.Properties.Resources._3f;
+            this.persas.Location = new System.Drawing.Point(429, 390);
+            this.persas.Name = "persas";
+            this.persas.Size = new System.Drawing.Size(49, 60);
+            this.persas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.persas.TabIndex = 6;
+            this.persas.TabStop = false;
+            this.persas.Click += new System.EventHandler(this.persas_Click);
+            // 
+            // doly
+            // 
+            this.doly.Image = global::EgitoCuriosidades.Properties.Resources._3d;
+            this.doly.Location = new System.Drawing.Point(264, 390);
+            this.doly.Name = "doly";
+            this.doly.Size = new System.Drawing.Size(49, 60);
+            this.doly.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.doly.TabIndex = 5;
+            this.doly.TabStop = false;
+            this.doly.Click += new System.EventHandler(this.doly_Click);
+            // 
+            // cats
+            // 
+            this.cats.Image = global::EgitoCuriosidades.Properties.Resources._2;
+            this.cats.Location = new System.Drawing.Point(374, 390);
+            this.cats.Name = "cats";
+            this.cats.Size = new System.Drawing.Size(49, 60);
+            this.cats.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cats.TabIndex = 4;
+            this.cats.TabStop = false;
+            this.cats.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // beard
+            // 
+            this.beard.Image = global::EgitoCuriosidades.Properties.Resources._3c;
+            this.beard.Location = new System.Drawing.Point(319, 390);
+            this.beard.Name = "beard";
+            this.beard.Size = new System.Drawing.Size(49, 60);
+            this.beard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.beard.TabIndex = 3;
+            this.beard.TabStop = false;
+            this.beard.Click += new System.EventHandler(this.beard_Click);
+            // 
+            // fundo
+            // 
+            this.fundo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fundo.Image = global::EgitoCuriosidades.Properties.Resources._3a;
+            this.fundo.Location = new System.Drawing.Point(0, 0);
+            this.fundo.Name = "fundo";
+            this.fundo.Size = new System.Drawing.Size(800, 450);
+            this.fundo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fundo.TabIndex = 2;
+            this.fundo.TabStop = false;
+            this.fundo.Click += new System.EventHandler(this.fundo_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "inicio";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(417, 116);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(61, 48);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(114, 420);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(61, 13);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Egito Video";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.persas);
+            this.Controls.Add(this.doly);
+            this.Controls.Add(this.cats);
+            this.Controls.Add(this.beard);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.fundo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.persas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.doly)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cats)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.beard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fundo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox fundo;
+        private System.Windows.Forms.PictureBox beard;
+        private System.Windows.Forms.PictureBox cats;
+        private System.Windows.Forms.PictureBox doly;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox persas;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ EgitoCuriosidades/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Globalization;
+using System.IO;
+using System.Media;
+
+
+namespace EgitoCuriosidades
+{
+    
+    public partial class Form1 : Form
+        
+    {
+        private SoundPlayer sound = new SoundPlayer(@"C:\Users\1988p\Desktop\programação\EgitoCuriosidades\projeto36\musicas\theme2.wav");
+        SpeechSynthesizer resposta = new SpeechSynthesizer();
+        List<string> testo_egito = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Parent = fundo;
+            pictureBox1.BackColor = Color.Transparent;
+            sound.Play();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sound.Stop();
+        }
+        private void desliga()
+        {
+            doly.Visible = false;
+            beard.Visible = false;
+            cats.Visible = false;
+            persas.Visible = false;
+        }
+        private void liga()
+        {
+            doly.Visible = true;
+            beard.Visible = true;
+            cats.Visible = true;
+            persas.Visible = true;
+        }
+       
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            desliga();
+            fundo.Load(@"C:\Users\1988p\Desktop\programação\EgitoCuriosidades\projeto36\fotos\2.jpg");
+            using (StreamReader ler = new StreamReader(@"C:\Users\1988p\Desktop\programação\EgitoCuriosidades\projeto36\testos\gatoDoEgito.txt"))
+            {
+                string linha = ler.ReadLine();
+                string[] valor = linha.Split('*');
+                testo_egito.Add(valor[0]);
+                foreach (var item in valor)
+                {
+                    resposta.SpeakAsync(item);
+                }
+            }
+
+        }
+
+        private void doly_Click(object sender, EventArgs e)
+        {
+            desliga();
+            fundo.Load(@"C:\Users\1988p\Desktop\programação\EgitoCuriosidades\projeto36\fotos\3d.jpg");
+            using (StreamReader ler = new StreamReader(@"C:\Users\1988p\Desktop\programação\EgitoCuriosidades\projeto36\testos\boneca.txt"))
+            {
+                string linha = ler.ReadLine();
+                string[] valor = linha.Split('*');
+                testo_egito.Add(valor[0]);
+                foreach (var item in valor)
+                {
+                    resposta.SpeakAsync(item);
+                }
+            }
+        }
+
+        private void beard_Click(object sender, EventArgs e)
+        {
+            desliga();
+            fundo.Load(@"C:\Users\1988p\Desktop\programação\EgitoCuriosidades\projeto36\fotos\3b.jpg");
+            using (StreamReader ler =new StreamReader(@"C:\Users\1988p\Desktop\programação\EgitoCuriosidades\projeto36\textos\boneca.txt"))
+            {
+                string linha = ler.ReadLine();
+                string[] valor = linha.Split('*');
+                testo_egito.Add(valor[0]);
+                foreach (var item in valor)
+                {
+                    resposta.SpeakAsync(item);
+                }
+            }
+            
+
+        }
+
+        private void persas_Click(object sender, EventArgs e)
+        {
+            desliga();
+            fundo.Load(@"C:\Users\1988p\Desktop\programação\EgitoCuriosidades\projeto36\fotos\3f.jpg");
+            using (StreamReader ler = new StreamReader(@"C:\Users\1988p\Desktop\programação\EgitoCuriosidades\projeto36\textos\persas.txt"))
+            {
+                string linha = ler.ReadLine();
+                string[] valor = linha.Split('*');
+                testo_egito.Add(valor[0]);
+                foreach (var item in valor)
+                {
+                    resposta.SpeakAsync(item);
+                }
+            }
+        }
+
+        private void fundo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            liga();
+            fundo.Load(@"C:\Users\1988p\Desktop\programação\EgitoCuriosidades\projeto36\fotos\3a.jpeg");
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("você ciclou no bico da piramide do meio");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=ebm-fLo9-NA");
+        }
+    }
+}
